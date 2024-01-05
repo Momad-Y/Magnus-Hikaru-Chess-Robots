997caa0635433be61eebe6ac8caca26c34235d7a
Mohamed Youssef <Mohamed.y.abdelnasser@gmail.com>
2024-01-05 20:59:48 +0200

    Add file description

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+"""
+This is the main file of the chess game. It creates a root window object and a chess game object.
+"""
+
 from GUI import chess_game  # Importing the chess game class from the GUI module
 import tkinter as tk  # Importing tkinter for the GUI window
 
